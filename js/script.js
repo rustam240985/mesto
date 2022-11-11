f018b634588c04046d27a1b18b3d4a3e09d263a3
Rustam Nigamatullin <rust2409@ya.ru>
2022-11-11 16:31:41 +0500

    refactor: добавить обработчики открытия и закрытия всех попапов

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close');
-const formElement = document.querySelector('.popup__form');
+const popupEditProfile = document.querySelector('.popup_action_edit-profile');
+const popupAddCard = document.querySelector('.popup_action_insert-card');
+const closePopupEditProfile = popupEditProfile.querySelector('.popup__close');
+const closePopupAddCard = popupAddCard.querySelector('.popup__close');
+const formElement = document.querySelector('.edit-profile-form');
+const formCard = document.querySelector('.add-card-form');
 const nameInput = formElement.querySelector('.popup__input_value_name');
 const jobInput = formElement.querySelector('.popup__input_value_profession');
+const nameCard = formCard.querySelector('.popup__input_value_place');
+const imageCard = formCard.querySelector('.popup__input_value_url');
 const elementsSection = document.querySelector('.elements');
 
 const profileName = document.querySelector('.profile__name');
 const profileProffesion = document.querySelector('.profile__profession');
 const editProfileButton = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button');
-
-/* const cardImages = document.querySelectorAll('.element__image');
-const cardTitles = document.querySelectorAll('.element__title'); */
 
 const elementTemplate = document.querySelector('#element-template').content;
 
@@ -42,15 +44,6 @@ const initialCards = [
   }
 ];
 
-/* for (let i = 0; i < cardImages.length; i += 1) {
-  cardImages[i].src = initialCards[i].link;
-  cardTitles[i].textContent = initialCards[i].name;
-} */
-
-
-
-/* cardImage.src = `${initialCards[0].link}` */
-
 function generateElement(dataElement) {
   const newElement = elementTemplate.querySelector('.element').cloneNode(true);
   const title = newElement.querySelector('.element__title');
@@ -66,35 +59,46 @@ function generateElement(dataElement) {
 }
 
 function renderElement(dataElement) {
-  elementsSection.append(generateElement(dataElement));
+  elementsSection.prepend(generateElement(dataElement));
 }
 
 initialCards.forEach((dataElement) => {
   renderElement(dataElement);
 })
 
-function formSubmitHandler(evt) {
+function formEditProfileSubmitHandler(evt) {
   evt.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileProffesion.textContent = jobInput.value;
   popupClose();
 }
 
-function popupOpen() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileProffesion.textContent;
-  popup.classList.add('popup_opened');
+function formAddCardSubmitHandler(evt) {
+  evt.preventDefault();
+  renderElement({ name: nameCard.value, link: imageCard.value });
+  popupClose();
 }
 
-function popupClose() {
-  popup.classList.remove('popup_opened');
+function popupOpen(event) {
+  if (event.target.classList.contains('profile__edit-button')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileProffesion.textContent;
+    popupEditProfile.classList.add('popup_opened');
+  }
+  else if (event.target.classList.contains('profile__add-button')) {
+    nameCard.value = '';
+    imageCard.value = '';
+    popupAddCard.classList.add('popup_opened')
+  }
 }
 
+function popupClose(event) {
+  document.querySelectorAll('.popup').forEach(popup => popup.classList.remove('popup_opened'))
+}
 
 editProfileButton.addEventListener('click', popupOpen);
-closePopupButton.addEventListener('click', popupClose);
-formElement.addEventListener('submit', formSubmitHandler);
-addCardButton.addEventListener('click', () => {
-
-})
+closePopupAddCard.addEventListener('click', popupClose);
+closePopupEditProfile.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formEditProfileSubmitHandler);
+formCard.addEventListener('submit', formAddCardSubmitHandler)
+addCardButton.addEventListener('click', popupOpen)
